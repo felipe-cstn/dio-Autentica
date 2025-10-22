6a5bf7e59e4dc6e476f55464e6a8351316726e04
Felipe-Celestino <felipe_celestino@yahoo.com.br>
2025-10-22 15:24:11 -0300

    Adicionando a soma de dois números

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int c = 10;
+int b = 30;
+int res = c + b;
+Console.WriteLine(c);
+Console.WriteLine(b);
+Console.WriteLine("", c, "+", b, "=", res);
+Console.WriteLine($"{c} + {b} = {res}");
+
